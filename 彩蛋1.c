80240766c04ad056f75684615873bc7a72ba6624
yuronfu <C14011073@mail.ncku.edu.tw>
2015-03-16 22:37:28 +0800

    找到蛋了
    改個副檔名

---
+++ 彩蛋1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int i;
+    
+    for(i = 1;i < 10;i++)
+    {
+                     printf("egg-%d\n",i); /* print egg-1
+                                                    egg-2
+                                                    ...
+                                                    egg-3
+                                           */
+    }
+    
+    getch();
+    return 0;
+}
